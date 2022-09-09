ce946a0186891dcd16ee256191b10ff98c87b670
AdejireA <adejireadegite@gmail.com>
2022-09-08 17:19:48 -0700

    Task 4 and 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints to the screen using puts function
+ * Return: If successful the exit status is zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
+
